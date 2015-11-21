62a4ce58b4225526bd544a985b143cb9252789b4
julietteTaka <juliettetuxbelin@gmail.com>
2015-11-21 21:04:34 +0100

    docker setup

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Starting mongodb"
+/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1 &
+
+echo "Starting Server"
+python ${ARTSPEAKER_DEV}/server/views.py > /opt/logs/catalog.log 2>&1 &
+echo "Starting Client"
+python ${ARTSPEAKER_DEV}/client/views.py > /opt/logs/client.log 2>&1 &
+echo "Done. Waiting..."
+
+wait
