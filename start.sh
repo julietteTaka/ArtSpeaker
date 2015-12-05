26d92cdaebd15b1e8c7368bcde83c7020f5332f0
julietteTaka <juliettetuxbelin@gmail.com>
2015-12-05 17:50:55 +0100

    minor fixes
    typo in index.html
    css in layout.html
    logfile name

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Starting mongodb"
 /opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1 &
 
 echo "Starting Server"
-python ${ARTSPEAKER_DEV}/server/views.py > /opt/logs/catalog.log 2>&1 &
+python ${ARTSPEAKER_DEV}/server/views.py > /opt/logs/server.log 2>&1 &
 echo "Starting Client"
 python ${ARTSPEAKER_DEV}/client/views.py > /opt/logs/client.log 2>&1 &
 echo "Done. Waiting..."
