4cc5f1bf47f7c3e80b3538aca8404f799f83b8b0
julietteTaka <juliettetuxbelin@gmail.com>
2016-01-30 00:21:44 +0100

    change directory emplacement in docker

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Starting mongodb"
-/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /opt/logs/mongo.log 2>&1 &
+/opt/mongodb/bin/mongod --dbpath /opt/mongo-data > /var/log/artspeaker/mongo.log 2>&1 &
 
 echo "Starting Server"
-python ${ARTSPEAKER_DEV}/server/views.py > /opt/logs/server.log 2>&1 &
+python ${ARTSPEAKER_DEV}/server/views.py > /var/log/artspeaker/server.log 2>&1 &
 echo "Starting Client"
-python ${ARTSPEAKER_DEV}/client/views.py > /opt/logs/client.log 2>&1 &
+python ${ARTSPEAKER_DEV}/client/views.py > /var/log/artspeaker/client.log 2>&1 &
 echo "Done. Waiting..."
 
 wait
