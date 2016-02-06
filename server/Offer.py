0edae7bc3b408dae59a8fa479f938b4c3ebd8e8d
julietteTaka <juliettetuxbelin@gmail.com>
2016-02-06 18:14:52 +0100

    new offer organisation

--- server/Offer.py
+++ server/Offer.py
@@ -1,28 +1,26 @@
 class Offer(object):
-    def __init__(self, offerId, userId, projectTitle):
+    def __init__(self, offerId, userId):
 
         # first step data
         self.userId = userId
         self.offerId = offerId
 
-        self.title = projectTitle
+        self.projectTitle = ""
         self.fieldActivity = None
         self.place = ""
         self.enterpriseLogo = None
         self.networking = []
         self.projectDate = {}
-
+        self.contact = {}
         self.isOpen = False
         self.isComplete = False
 
-
         # second step data
+        self.offerTitle = ""
         self.offerDate = {}
-        self.wantedProfiles = []
+        self.wantedProfiles = ""
         self.text = ""
         self.tags = []
-        self.sector = []
-        self.contact = {}
 
         # not sure
         self.remuneration = None
