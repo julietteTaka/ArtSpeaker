90ccb794a2a6fef24e489c5834d992264e87a332
julietteTaka <juliettetuxbelin@gmail.com>
2016-02-12 21:47:32 +0100

    get offer by id

--- client/views.py
+++ client/views.py
@@ -108,6 +108,15 @@ def deleteOffer(offerID):
         abort(result.status_code,  {'message': 'il y a eu une erreur lors la suppression de l\'offre.'})
     return jsonify(**result.json())
 
+@config.g_app.route('/offer/<offerID>', methods=['GET'])
+def getOfferById(offerID):
+    result = requests.get(config.serverRootUri + '/offer/' + offerID)
+    if result.status_code != 200:
+        abort(result.status_code,  {'message': 'can\'t retrieve offer '+offerID})
+    if result is None:
+        return render_template("index.html") # toDo page erreur
+    return jsonify(**result.json())
+
 @config.g_app.route('/offer/<offerId>/step/<step>', methods=['POST'])
 def completeOffer(offerId, step):
     header = {'content-type' : 'application/json'}
